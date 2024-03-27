e9304805e21e3f6f152aa23ba0b12f35ce5058ab
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-27 21:38:56 +0800

    Update rileyhubarsenal.lua

--- rileyhubarsenal.lua
+++ rileyhubarsenal.lua
@@ -2,42 +2,16 @@ local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/blood
 
 local PhantomForcesWindow = Library:NewWindow("Arsenal Stuff")
 
-local Arsenal = PhantomForcesWindow:NewSection("Arsenal")
+local KillingCheats = PhantomForcesWindow:NewSection("Kill Options")
 
-Arsenal:CreateButton("Inf Ammo", function()
-while wait() do
-        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
-        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
-    end
-end)
+KillingCheats:CreateButton("Inf Ammo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rileybeerblx2/Aimbot-Lua/main/infiniteammoarsenal.lua"))()
 end)
 
-Arsenal:CreateButton("RGB Gun", function()
-local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
-end)
+KillingCheats:CreateButton("RGB Gun", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rileybeerblx2/Aimbot-Lua/main/rgbgunarsenal.lua"))()
 end)
 
-Arsenal:CreateButton("Rapid Fire", function()
-local replicationstorage = game.ReplicatedStorage
-for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
-   if v.Name == "Auto" then
-       v.Value = true
-   end
-   if v.Name == "RecoilControl" then
-       v.Value = 0
-   end
-   if v.Name == "MaxSpread" then
-       v.Value = 0
-   end
-   if v.Name == "ReloadTime" then
-      v.Value = 0
-   end
-   if v.Name == "FireRate" then
-       v.Value = 0.05
-   end
-   if v.Name == "Crit" then
-       v.Value = 20
-   end
-end
-end)
+KillingCheats:CreateButton("Rapid Fire", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rileybeerblx2/Aimbot-Lua/main/fasterfireraterapidfirearsenal.lua"))()
 end)
