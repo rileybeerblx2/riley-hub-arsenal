d691e39a37742df263f965384676afb851b87613
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-27 21:31:59 +0800

    Update rileyhubarsenal.lua

--- rileyhubarsenal.lua
+++ rileyhubarsenal.lua
@@ -2,7 +2,7 @@ local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/blood
 
 local PhantomForcesWindow = Library:NewWindow("Arsenal Stuff")
 
-Arsenal:CreateButton("Inf Ammo", function()
+KillingCheats:CreateButton("Inf Ammo", function()
 while wait() do
         game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
         game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
@@ -10,12 +10,12 @@ while wait() do
 end)
 end)
 
-Arsenal:CreateButton("RGB Gun", function()
+KillingCheats:CreateButton("RGB Gun", function()
 local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
 end)
 end)
 
-Arsenal:CreateButton("Rapid Fire", function()
+KillingCheats:CreateButton("Rapid Fire", function()
 local replicationstorage = game.ReplicatedStorage
 for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
    if v.Name == "Auto" then
