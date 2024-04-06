5ffe6c9c52b9e812c8b481b100022e16223973d2
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-04-06 20:14:26 +0800

    Create fullbright.lua

---
+++ fullbright.lua
@@ -0,0 +1,9 @@
+local aa=Instance.new("PointLight",game.Players.LocalPlayer.Character.HumanoidRootPart)
+																																										  aa.Brightness=.3;
+																																										  aa.Range=10000;
+																																										  E.TimeOfDay="14:00:00"
+																																										  E.FogEnd=10000;
+																																										  E.Brightness=2;
+																																										  E.Ambient=Color3.fromRGB(255,255,255)
+																																										  E.FogColor=Color3.fromRGB(255,255,255)
+																																										  end)
